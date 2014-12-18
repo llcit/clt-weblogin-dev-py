3353fa8343eb8577b86ab0a5adc34e9f8d937ddf
Richard Medina <rmedina@Ruby.local>
2014-12-18 13:53:20 -1000

    fabfile committed this without comments.

--- webloginsite/urls.py
+++ webloginsite/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from siteapp.views import HomeView
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'webloginsite.views.home', name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+	url(r'^accounts/login$', 'django_cas_ng.views.login'),
+    url(r'^accounts/logout$', 'django_cas_ng.views.logout'),
     url(r'^admin/', include(admin.site.urls)),
 )
