958c7bf93da505740d2b5b527c6b75d3b33e9e91
Richard Medina <rmedina@Ruby.local>
2014-12-19 15:04:24 -1000

    fabfile committed this without comments.

--- webloginsite/urls.py
+++ webloginsite/urls.py
@@ -7,7 +7,12 @@ urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-	url(r'^accounts/login$', 'django_cas.views.login'),
-    url(r'^logout$', 'django_cas.views.logout'),
+
+	url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+	
+	# url(r'^accounts/login$', 'django_cas.views.login'),
+ #    url(r'^logout$', 'django_cas.views.logout'),
+    
     url(r'^admin/', include(admin.site.urls)),
 )
